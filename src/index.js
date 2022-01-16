a2e9c6c10c9fde0ec586f53f842977dfcd61caa8
Piloniak <xxziomcioxx@gmail.com>
2022-01-16 16:27:55 +0100

    move buttons

--- src/index.js
+++ src/index.js
@@ -2,66 +2,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import React from "react";
 
-class Game extends React.Component {
- constructor(props) {
-    super(props);
-
-    this.state = {
-      board: null,
-    };
-  }
-
-  initBoard() {
-    let board = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
-    board = this.placeRandom(this.placeRandom(board));
-    this.setState({board});
-  }
-
-  getBlankCoordinates(board) {
-    const blankCoordinates = [];
-
-    for (let r = 0; r < board.length; r++) {
-      for (let c = 0; c < board[r].length; c++) {
-        if (board[r][c] === 0) {blankCoordinates.push([r, c])}
-      }
-    }
-
-    return blankCoordinates;
-  }
-
-  randomStartingNumber() {
-    const startingNumbers = [2,4];
-    const randomNumber = startingNumbers[Math.floor(Math.random() * startingNumbers.length)];
-    return randomNumber;
-  }
-
-  placeRandom(board) {
-    const blankCoordinates = this.getBlankCoordinates(board);
-    const randomCoordinate = blankCoordinates[Math.floor(Math.random() * blankCoordinates.length)];
-    const randomNumber = this.randomStartingNumber();
-    board[randomCoordinate[0]][randomCoordinate[1]] = randomNumber;
-    return board;
-  }
-
-  componentWillMount() {
-    this.initBoard();
-    const body = document.querySelector('body');
-    body.addEventListener('keydown', this.handleKeyDown.bind(this));
-  }
-
-  handleKeyDown(e) {
-  }
-
-  render() {
-    return (
-      <div>
-        <table>
-          {this.state.board.map((row, i) => (<Row key={i} row={row} />))}
-        </table>
-      </div>
-    );
-  }
-};
 
 const Row = ({ row }) => {
   return (
@@ -83,4 +23,298 @@ const Cell = ({ cellValue }) => {
     </td>
   );
 };
+
+
+class Game extends React.Component {
+ constructor(props) {
+    super(props);
+    this.state = {
+      board: null,
+    };
+  }
+    
+  getBlankCoordinates(board) {
+    const blankCoordinates = [];
+    
+    for (let r = 0; r < board.length; r++) {
+      for (let c = 0; c < board[r].length; c++) {
+        if (board[r][c] === 0) {blankCoordinates.push([r, c])}
+      }
+    }
+            
+    return blankCoordinates;
+  }
+  
+  randomStartingNumber() {
+    const startingNumbers = [2,4];
+    const randomNumber = startingNumbers[Math.floor(Math.random() * startingNumbers.length)];
+    return randomNumber;
+  }
+  
+  placeRandom(board) {
+    const blankCoordinates = this.getBlankCoordinates(board);
+    const randomCoordinate = blankCoordinates[Math.floor(Math.random() * blankCoordinates.length)];
+    const randomNumber = this.randomStartingNumber();
+    board[randomCoordinate[0]][randomCoordinate[1]] = randomNumber;
+    return board;
+  }
+
+  initBoard() {
+    let board =
+     [
+      [0,0,0,0],
+      [0,0,0,0],
+      [0,0,0,0],
+      [0,0,0,0]
+    ];
+    board = this.placeRandom(this.placeRandom(board));
+    this.setState({board});
+  }
+
+  
+  boardMoved(original, updated) {
+    return (JSON.stringify(updated) !== JSON.stringify(original)) ? true : false;
+  }
+  
+  
+  move(direction) {
+    if (!this.state.gameOver) {
+      if (direction === 'up') {
+        const movedUp = this.moveUp(this.state.board);
+        if (this.boardMoved(this.state.board, movedUp.board)) {
+          const upWithRandom = this.placeRandom(movedUp.board);
+          
+          if (this.checkForGameOver(upWithRandom)) {
+            this.setState({board: upWithRandom, gameOver: true, message: 'Game over!'});
+          } else {
+            this.setState({board: upWithRandom});  
+          }
+        }
+      } else if (direction === 'right') {
+        const movedRight = this.moveRight(this.state.board);
+        if (this.boardMoved(this.state.board, movedRight.board)) {
+          const rightWithRandom = this.placeRandom(movedRight.board);
+          
+          if (this.checkForGameOver(rightWithRandom)) {
+            this.setState({board: rightWithRandom, gameOver: true, message: 'Game over!'});
+          } else {
+            this.setState({board: rightWithRandom});  
+          }
+        }
+      } else if (direction === 'down') {
+        const movedDown = this.moveDown(this.state.board);
+        if (this.boardMoved(this.state.board, movedDown.board)) {
+          const downWithRandom = this.placeRandom(movedDown.board);
+          
+          if (this.checkForGameOver(downWithRandom)) {
+            this.setState({board: downWithRandom, gameOver: true, message: 'Game over!'});
+          } else {
+            this.setState({board: downWithRandom});
+          }
+        }
+      } else if (direction === 'left') {
+        const movedLeft = this.moveLeft(this.state.board);
+        if (this.boardMoved(this.state.board, movedLeft.board)) {
+          const leftWithRandom = this.placeRandom(movedLeft.board);
+          
+          if (this.checkForGameOver(leftWithRandom)) {
+            this.setState({board: leftWithRandom, gameOver: true, message: 'Game over!'});  
+          } else {
+            this.setState({board: leftWithRandom});
+          }
+        }
+      }
+    } else {
+      this.setState({message: 'Game over. Please start a new game.'});
+    }
+  }
+  
+  moveUp(inputBoard) {
+    let rotatedRight = this.rotateRight(inputBoard);
+    let board = [];
+
+   
+    for (let r = 0; r < rotatedRight.length; r++) {
+      let row = [];
+      for (let c = 0; c < rotatedRight[r].length; c++) {
+        let current = rotatedRight[r][c];
+        (current === 0) ? row.unshift(current) : row.push(current);
+      }
+      board.push(row);
+    }
+
+   
+    for (let r = 0; r < board.length; r++) {
+      for (let c = board[r].length - 1; c >= 0; c--) {
+        if (board[r][c] > 0 && board[r][c] === board[r][c - 1]) {
+          board[r][c] = board[r][c] * 2;
+          board[r][c - 1] = 0;
+        } else if (board[r][c] === 0 && board[r][c - 1] > 0) {
+          board[r][c] = board[r][c - 1];
+          board[r][c - 1] = 0;
+        }
+      }
+    }
+
+    
+    board = this.rotateLeft(board);
+
+    return {board};
+  }
+  
+  moveRight(inputBoard) {
+    let board = [];
+
+    
+    for (let r = 0; r < inputBoard.length; r++) {
+      let row = [];      
+      for (let c = 0; c < inputBoard[r].length; c++) {
+        let current = inputBoard[r][c];
+        (current === 0) ? row.unshift(current) : row.push(current);
+      }
+      board.push(row);
+    }
+
+   
+    for (let r = 0; r < board.length; r++) {
+      for (let c = board[r].length - 1; c >= 0; c--) {
+        if (board[r][c] > 0 && board[r][c] === board[r][c - 1]) {
+          board[r][c] = board[r][c] * 2;
+          board[r][c - 1] = 0;
+        } else if (board[r][c] === 0 && board[r][c - 1] > 0) {
+          board[r][c] = board[r][c - 1];
+          board[r][c - 1] = 0;
+        }
+      }
+    }
+
+    return {board};
+  }
+  
+  moveDown(inputBoard) {
+    let rotatedRight = this.rotateRight(inputBoard);
+    let board = [];
+
+    for (let r = 0; r < rotatedRight.length; r++) {
+      let row = [];      
+      for (let c = rotatedRight[r].length - 1; c >= 0; c--) {
+        let current = rotatedRight[r][c];
+        (current === 0) ? row.push(current) : row.unshift(current);
+      }
+      board.push(row);
+    }
+
+    
+    for (let r = 0; r < board.length; r++) {
+      for (let c = 0; c < board.length; c++) {
+        if (board[r][c] > 0 && board[r][c] === board[r][c + 1]) {
+          board[r][c] = board[r][c] * 2;
+          board[r][c + 1] = 0;
+        } else if (board[r][c] === 0 && board[r][c + 1] > 0) {
+          board[r][c] = board[r][c + 1];
+          board[r][c + 1] = 0;
+        }
+      }
+    }
+
+  
+    board = this.rotateLeft(board);
+
+    return {board, };
+  }
+  
+  moveLeft(inputBoard) {
+    let board = [];
+
+    
+    for (let r = 0; r < inputBoard.length; r++) {
+      let row = [];      
+      for (let c = inputBoard[r].length - 1; c >= 0; c--) {
+        let current = inputBoard[r][c];
+        (current === 0) ? row.push(current) : row.unshift(current);
+      }
+      board.push(row);
+    }
+
+    
+    for (let r = 0; r < board.length; r++) {
+      for (let c = 0; c < board.length; c++) {
+        if (board[r][c] > 0 && board[r][c] === board[r][c + 1]) {
+          board[r][c] = board[r][c] * 2;
+          board[r][c + 1] = 0;
+        } else if (board[r][c] === 0 && board[r][c + 1] > 0) {
+          board[r][c] = board[r][c + 1];
+          board[r][c + 1] = 0;
+        }
+      }
+    }
+    
+    return {board};
+  }
+  
+  rotateRight(matrix) {
+    let result = [];
+	
+  	for (let c = 0; c < matrix.length; c++) {
+	  	let row = [];
+	  	for (let r = matrix.length - 1; r >= 0; r--) {
+			  row.push(matrix[r][c]);
+		  }
+      result.push(row);
+	  }
+	
+	  return result;
+  }
+  
+  rotateLeft(matrix) {
+  	let result = [];
+
+    for (let c = matrix.length - 1; c >= 0; c--) {
+      let row = [];
+      for (let r = matrix.length - 1; r >= 0; r--) {
+        row.unshift(matrix[r][c]);
+      }
+      result.push(row);
+    }
+
+    return result;
+  }
+  
+  checkForGameOver(board) {
+    let moves = [
+      this.boardMoved(board, this.moveUp(board).board),
+      this.boardMoved(board, this.moveRight(board).board),
+      this.boardMoved(board, this.moveDown(board).board),
+      this.boardMoved(board, this.moveLeft(board).board)
+    ];
+    
+    return (moves.includes(true)) ? false : true;
+  }
+  
+  componentWillMount() {
+    this.initBoard();  
+    const body = document.querySelector('body');
+    
+  }
+  
+ 
+    
+  render() {
+    return (
+      <div>   
+      <h1>Przemek Zaława & Mateusz Cupiał - 2048</h1>                     
+        <table>
+          {this.state.board.map((row, i) => (<Row key={i} row={row} />))}
+        </table>
+        <div className="buttons">
+          <button className="button" onClick={() => {this.move('up')}}>Up</button>
+          <button className="button" onClick={() => {this.move('right')}}>Right</button>
+          <button className="button" onClick={() => {this.move('down')}}>Down</button>
+          <button className="button" onClick={() => {this.move('left')}}>Left</button>
+        </div>      
+      </div>
+    );
+  }
+};
+
 ReactDOM.render(<Game />, document.getElementById('root'));
